e2097e0ae15f5936fc1f5097cffcf0fd62c3e0cf
Rob Hoover <rtrhoover@gmail.com>
2022-05-22 15:36:38 -0700

    build projects page and project data array

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        title: "Facial Recognition App",
+        subtitle: "React, Node.js, PostgreSQL",
+        description: 
+            "An app built with React on the front end, a server built with Node.js with a secure user registration feature that is stored on a database built with PostgreSQL.  The app itself is an image uploader to identify faces using the Clarifai API",
+        image: "./assets/FacialRecognitionProject.jpg",
+        link: "https://facial-recognition-app-react.herokuapp.com/"
+    }
+];
