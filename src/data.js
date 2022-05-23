61cb611da20775a141a30ffacc40ecd068813b85
Rob Hoover <rtrhoover@gmail.com>
2022-05-22 23:53:03 -0700

    complete project file and expand project database

--- src/data.js
+++ src/data.js
@@ -6,5 +6,29 @@ export const projects = [
             "An app built with React on the front end, a server built with Node.js with a secure user registration feature that is stored on a database built with PostgreSQL.  The app itself is an image uploader to identify faces using the Clarifai API",
         image: "./assets/FacialRecognitionProject.jpg",
         link: "https://facial-recognition-app-react.herokuapp.com/"
-    }
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+        link: "https://reedbarger.com",
+      },
+      {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+        link: "https://jsbootcamp.com",
+      },
+      {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+      }
 ];
