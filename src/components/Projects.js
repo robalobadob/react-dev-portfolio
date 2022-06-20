7cafe9711c545d3af6619c1e9a97b329fb37f8c3
Rob Hoover <rtrhoover@gmail.com>
2022-06-20 15:30:27 -0700

    added rel referrer to blank target link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,6 +22,7 @@ export default function Projects() {
                         <a
                             href={project.link}
                             target="_blank"
+                            rel="noreferrer"
                             key={project.image}
                             className="sm:w-1/2 w-100 p-4">
                             <div className="flex relative">
