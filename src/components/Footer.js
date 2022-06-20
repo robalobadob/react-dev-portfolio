b8af66ba8257edc1e7395ef21b5272ca371f6ec9
Rob Hoover <rtrhoover@gmail.com>
2022-06-20 14:47:40 -0700

    footer css update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,19 @@ import { FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi"
 export default function Footer() {
     return (
         <footer className="bg-teal-800">
-            <div>
-                <nav className="container mx-auto flex text-center">
-                    <a className="mr-5 text-4xl text-white hover:text-orange-600" href="https://github.com/robalobadob/" target="_blank" rel="noreferrer">
-                        <FiGithub />
-                    </a>
-                    
-                    <a className="mr-5 text-4xl text-white hover:text-orange-600" href="https://www.linkedin.com/in/rob-hoover/" target="_blank" rel="noreferrer">
-                        <FiLinkedin />
-                    </a>
-                    
-                    <a className="mr-5 text-4xl text-white hover:text-orange-600" href="https://www.instagram.com/robalobadob/" target="_blank" rel="noreferrer">
-                        <FiInstagram />
-                    </a>
-                </nav>
-            </div>
+            <nav className="container mx-auto w-1/2 flex p-5 items-center">
+                <a className="mx-auto text-4xl text-white hover:text-orange-600" href="https://github.com/robalobadob/" target="_blank" rel="noreferrer">
+                    <FiGithub />
+                </a>
+                
+                <a className="mx-auto text-4xl text-white hover:text-orange-600" href="https://www.linkedin.com/in/rob-hoover/" target="_blank" rel="noreferrer">
+                    <FiLinkedin />
+                </a>
+                
+                <a className="mx-auto text-4xl text-white hover:text-orange-600" href="https://www.instagram.com/robalobadob/" target="_blank" rel="noreferrer">
+                    <FiInstagram />
+                </a>
+            </nav>
         </footer>
     )
 }
