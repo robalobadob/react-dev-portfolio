e880026572403067b5905ee255c1c259d3b27629
Rob <rtrhoover@gmail.com>
2024-12-02 22:52:13 -0800

    adds info for new projects - vc, sound, evoke

--- src/data.js
+++ src/data.js
@@ -1,84 +1,82 @@
-import projectImage1 from "../src/assets/FacialRecognitionProject.jpg";
-import projectImage2 from "../src/assets/AnchoressWebSite.jpg";
-import projectImage3 from "../src/assets/RobofriendsApp.jpg";
-import projectImage4 from "../src/assets/EarlyOnsetRecordsSite.jpg";
-import projectImage5 from "../src/assets/GardennsWebsite.jpg";
+import projectImage1 from "../src/assets/Visual-Capitalist.png";
+import projectImage2 from "../src/assets/The-Sound.png";
+import projectImage3 from "../src/assets/Evoke.png";
+import projectImage4 from "../src/assets/AnchoressWebSite.jpg";
+import projectImage5 from "../src/assets/RobofriendsApp.jpg";
+import projectImage6 from "../src/assets/EarlyOnsetRecordsSite.jpg";
+import projectImage7 from "../src/assets/GardennsWebsite.jpg";
 
 export const projects = [
-    {
-        title: "Facial Recognition App",
-        subtitle: "React, Express.js, PostgreSQL",
-        description: 
-            "An app built with React.js on the front end, a RESTful API server built with Express.js and deployed on Heroku. It has session management with authenticated routes, with user information and hashed passwords stored securely in a database with PostgreSQL.",
-        image: projectImage1,
-        link: "https://facial-recognition-app-react.herokuapp.com/"
-    },
-    {
-        title: "Artist Website",
-        subtitle: "Bootstrap 5, SCSS",
-        description:
-          "A multipage website for a band, built using Bootstrap and SCSS before bundling with Parcel. Hosted via cPanel. There are multiple iframes pulled from Bandcamp and YouTube. Site also includes a hidden page that must be linked to directly that is also blocked from search indexing.",
-        image: projectImage2,
-        link: "https://anchoress.ca",
-      },
-      {
-        title: "Robofriends",
-        subtitle: "React / Redux, Tachyons",
-        description:
-          "A stateful app built initially with React.js before being reformated with Redux that offers a search function for example datasets provided by multiple APIs. Styled using Tachyons CSS.",
-        image: projectImage3,
-        link: "https://github.com/robalobadob/robofriends/",
-      },
-      {
-        title: "Record Label Website",
-        subtitle: "Shopify, Liquid",
-        description:
-          "A startup record label ecommerce platform by artists for artists, built with a Shopify template and then customized with Liquid. Primarily based around a vinyl subscription model with additional merchandise items for individual sale. Also set up a private Discord for artists and admin.",
-        image: projectImage4,
-        link: "https://earlyonsetrecords.com/",
-      },
-      {
-        title: "Client Website",
-        subtitle: "Bootstrap 5, SCSS",
-        description:
-          "A small business website for a client, built using Bootstrap and SCSS before bundling with Parcel.",
-        image: projectImage5,
-        link: "https://gardenns.com",
-      }
+  {
+    title: "Visual Capitalist",
+    subtitle: "PHP, HTML, CSS, JS, MySQL",
+    description: "Visual Capitalist is an online publisher with an average monthly audience of 5 million users. I design and develop pages, optimize network and database infrastructure, oversee site security, and handle a variety of revenue generating tools such as our premium subscription services and programmatic advertising.",
+    image: projectImage1,
+    link: "https://www.visualcapitalist.com/",
+  },
+  {
+    title: "The Sound",
+    subtitle: "PHP, HTML, CSS, JS, MySQL",
+    description: "The Sound is an international design agency. I maintain the site infrastructure creating custom backend tools for the team, while optimizing the front end for Core Web Vitals and SEO.",
+    image: projectImage2,
+    link: "https://www.thesoundhq.com/",
+  },
+  {
+    title: "Evoke International Design",
+    subtitle: "JS, Liquid, CSS",
+    description: "The Sound is an Vancouver-based interior design and graphic agency. I have worked on a variety of client projects from architectural firms to photographer portfolios to help them reach their full potential.",
+    image: projectImage3,
+    link: "https://www.thesoundhq.com/",
+  },
+  {
+    title: "Artist Website",
+    subtitle: "Bootstrap 5, SCSS",
+    description: "A multipage website for a band, built using Bootstrap and SCSS before bundling with Parcel. Hosted via cPanel. There are multiple iframes pulled from Bandcamp and YouTube. Site also includes a hidden page that must be linked to directly that is also blocked from search indexing.",
+    image: projectImage4,
+    link: "https://anchoress.ca",
+  },
+  {
+    title: "Robofriends",
+    subtitle: "React / Redux, Tachyons",
+    description: "A stateful app built initially with React.js before being reformated with Redux that offers a search function for example datasets provided by multiple APIs. Styled using Tachyons CSS.",
+    image: projectImage5,
+    link: "https://github.com/robalobadob/robofriends/",
+  },
+  {
+    title: "Record Label Website",
+    subtitle: "Shopify, Liquid",
+    description: "A startup record label ecommerce platform by artists for artists, built with a Shopify template and then customized with Liquid. Primarily based around a vinyl subscription model with additional merchandise items for individual sale. Also set up a private Discord for artists and admin.",
+    image: projectImage6,
+    link: "https://earlyonsetrecords.com/",
+  },
+  {
+    title: "Client Website",
+    subtitle: "Bootstrap 5, SCSS",
+    description: "A small business website for a client, built using Bootstrap and SCSS before bundling with Parcel.",
+    image: projectImage7,
+    link: "https://gardenns.com",
+  },
 ];
 
-export const skills = [
-    "Web Development",
-    "HTML/CSS/JS",
-    "React",
-    "Testing/Debugging",
-    "PostgreSQL",
-    "Web Design",
-    "UX/UI",
-    "Digital Marketing",
-    "SEO"
-];
+export const skills = ["Web Development", "HTML/CSS/JS", "React", "Testing/Debugging", "PostgreSQL", "Web Design", "UX/UI", "Digital Marketing", "SEO"];
 
 export const testimonial = [
-    {
-        quote: 
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: projectImage1,
-        name: "Airrick Dunfield",
-        company: "BCIT"
-    },
-    {
-      quote:
-          "Gosh wow, what a great dude and a swell developer!",
-      image: projectImage4,
-      name: "Ryan Dolejsi",
-      company: "Early Onset Records"
-    },
-    {
-        quote:
-            "Gosh wow, what a great dude and a swell developer!",
-        image: projectImage5,
-        name: "Kim Enns",
-        company: "GardEnns"
-    }
-]
+  {
+    quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: projectImage1,
+    name: "Airrick Dunfield",
+    company: "BCIT",
+  },
+  {
+    quote: "Gosh wow, what a great dude and a swell developer!",
+    image: projectImage4,
+    name: "Ryan Dolejsi",
+    company: "Early Onset Records",
+  },
+  {
+    quote: "Gosh wow, what a great dude and a swell developer!",
+    image: projectImage5,
+    name: "Kim Enns",
+    company: "GardEnns",
+  },
+];
